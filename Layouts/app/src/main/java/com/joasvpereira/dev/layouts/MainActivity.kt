0ac27fd87e786c6ca8625ce15fb49e531ae6847d
joas.pereira <joasdavid@gmail.com>
2021-11-03 16:25:31 +0000

    code for chapter 6

--- Layouts/app/src/main/java/com/joasvpereira/dev/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/joasvpereira/dev/layouts/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -26,6 +34,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,7 +42,10 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.joasvpereira.dev.layouts.ui.theme.LayoutsTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,11 +91,63 @@ fun ToolbarActions() {
 
 @Composable
 private fun BodyContent(modifier: Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
-        Spacer(modifier = Modifier.padding(top = 18.dp))
-        PhotographerCard()
+    val listScrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    val listSize = 100
+
+    Column(Modifier.fillMaxWidth()) {
+
+        Row(Modifier.fillMaxWidth()) {
+            Button(
+                onClick = scrollToTopClickEvent(coroutineScope, listScrollState),
+                modifier = Modifier.weight(0.5f)
+            ) {
+                Text(text = "Scroll to the top")
+            }
+
+            Button(
+                onClick = scrollToBottomClickEvent(coroutineScope, listScrollState, listSize),
+                modifier = Modifier.weight(0.5f)
+            ) {
+                Text(text = "Scroll to the bottom")
+            }
+        }
+
+        LazyColumn(modifier = Modifier.fillMaxWidth() then modifier, state = listScrollState) {
+            item {
+                Text(text = "Hi there!")
+                Text(text = "Thanks for going through the Layouts codelab")
+                Spacer(modifier = Modifier.padding(top = 18.dp))
+            }
+            SimpleList(scope = this, listSize = listSize)
+        }
+    }
+}
+
+@Composable
+private fun scrollToTopClickEvent(
+    coroutineScope: CoroutineScope,
+    listScrollState: LazyListState
+): () -> Unit = {
+    coroutineScope.launch {
+        listScrollState.animateScrollToItem(index = 0)
+    }
+}
+
+@Composable
+private fun scrollToBottomClickEvent(
+    coroutineScope: CoroutineScope,
+    listScrollState: LazyListState,
+    listSize: Int
+): () -> Unit = {
+    coroutineScope.launch {
+        listScrollState.animateScrollToItem(index = listSize - 1)
+    }
+}
+
+fun SimpleList(listSize: Int, scope: LazyListScope) {
+    scope.items(listSize) { pos: Int ->
+        PhotographerCard(name = "Item #$pos")
     }
 }
 
@@ -117,7 +181,11 @@ fun PhotographerAvatar() {
         shape = CircleShape,
         color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
     ) {
-
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
@@ -133,7 +201,7 @@ private fun PhotographerCardDisplayInfo(modifier: Modifier, name: String) {
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun PhotographerCardPreview() {
     val context = LocalContext.current
@@ -146,7 +214,7 @@ fun PhotographerCardPreview() {
     }
 }
 
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     val context = LocalContext.current
